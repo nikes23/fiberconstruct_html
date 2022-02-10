65071449a39f56189db6b740d27d48008c05f86f
nikes23 <nikes23@hotmail.de>
2022-02-10 13:43:28 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const hamburger = document.querySelector('.hamburger');
+const navElements = document.querySelector('.navElements');
+const navLink = document.querySelector('.nav__link');
+
+hamburger.addEventListener('click', () => {
+    navLink.classList.toggle('hide');
+});
+navElements.addEventListener('click', () => {
+    navLink.classList.toggle('hide');
+});
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+
+const nav = document.querySelector('#nav');
+let navTop = nav.offsetTop;
+
+function buttonPressFunction() {
+    document.getElementById("button1").style.display = "none";
+}
