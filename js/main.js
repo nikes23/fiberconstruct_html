1a7ecdd20ed479e5175356eac97e16883c4ac1b8
nikes23 <nikes23@hotmail.de>
2022-02-17 17:33:04 +0100

    Version 1.0.0

--- js/main.js
+++ js/main.js
@@ -21,3 +21,49 @@ let navTop = nav.offsetTop;
 function buttonPressFunction() {
     document.getElementById("button1").style.display = "none";
 }
+
+
+//Call and Calender Button
+//Mobile Variable
+const phoneCall = document.getElementById("phone");
+const xPhone = document.getElementById("xPhone");
+const callBackMobil = document.getElementById("callBackMobil");
+const callNowMobil = document.getElementById("callNowMobil");
+//Tablet + Desktop Variable
+const calender = document.getElementById("calender");
+const xDesktop = document.getElementById("xDesktop");
+//const callBackDesktop = document.getElementById("callBackDesktop");
+
+//Change Button Icon and remove Textfields
+window.addEventListener('resize', function() {
+    callBackMobil.style.display = 'none';
+    callNowMobil.style.display = 'none';
+    //callBackDesktop.style.display = 'none';
+
+    if(xPhone.style.display === 'inline-block'){
+        xPhone.style.display = 'none';
+        phoneCall.style.display = 'inline-block';
+    } else if(xDesktop.style.display === 'inline-block'){
+        xDesktop.style.display = 'none';
+        calender.style.display = 'inline-block';
+    } else{
+        return true;
+    }
+});
+
+//Show and hide Textfields on Smartphone and change Button Icon
+function phoneFunction(x) {
+    if (phoneCall.style.display === 'none') {
+        x.classList.toggle("change");
+        xPhone.style.display = 'none';
+        callBackMobil.style.display = 'none';
+        callNowMobil.style.display = 'none';
+        phoneCall.style.display = 'inline-block';
+    } else {
+        x.classList.toggle("change");
+        xPhone.style.display = 'inline-block';
+        callBackMobil.style.display = 'inline-block';
+        callNowMobil.style.display = 'inline-block';
+        phoneCall.style.display = 'none';
+    }
+}
